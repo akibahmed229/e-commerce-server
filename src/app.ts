f72b0ef3508655258c96db927936ffdf58c19729
akibahmed229 <akib4418@gmail.com>
2026-07-28 12:28:10 +0600

    default '/' get route added & route not found added

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import { errorHandler } from "@core/middlewares/error-handler";
 import { logger } from "@core/middlewares/log-events";
 import { productRoutes } from "@features/product/presentation/product.routes";
 import { userRoutes } from "@features/user/presentation/user.routes";
-import express, { type Express } from "express"
+import express, { type Express, type Request, type Response } from "express"
+import path from "path"
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { orderRoutes } from "@features/order/presentation/order.routes";
@@ -20,12 +21,29 @@ app.use(cors(corsOptions));
 // handles its own raw() parsing internally via payment.routes.ts
 app.use(byPassExpressJson);
 
+//serve static files
+app.use("/", express.static(path.join(__dirname, "../", "public")));
 
+// get route for homepage match: /, /index, /index.html
+app.get(/^\/$|^\/index(.html)?$/, (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../", "views", "index.html"));
+});
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/orders", orderRoutes);
 app.use("/api/v1/payments", paymentRoutes);
 
-app.use(errorHandler);
+// fallback for 404
+app.use((req: Request, res: Response) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "../", "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
+});
 
+app.use(errorHandler);
